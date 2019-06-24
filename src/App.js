27267d219025efe25fc3a3dafa2f889d8335d6f9
PK <prafull.karanje@bitstreet.co.in>
2019-06-24 15:35:23 +0530

    With truncated text

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import './App.scss';
 function App() {
   return (
     <div className="p-3">
-    <div className="listItem white d-flex justify-content-between bg-white py-2 px-3">
-      <div>
-        <div className="listItem__status font-weight-bold"> Warning </div>
-        <div className="text-black-50" > sdfjsdfjsdfksdflsdf;</div>
+      <div className="listItem white d-flex justify-content-between bg-white py-2 px-3 shadow-sm my-2">
+        <div className="listItem__body text-left d-flex flex-column justify-content-between ">
+          <div className="listItem__status font-weight-bold"> Warning </div>
+          <div className="text-black-50 listItem__msg" > sdfjsdfjssfgsdgsgsgsgsgsdgsdgsgdfksdflsdf;</div>
+        </div>
+        <button className="btn btn-link px-0 text-black-50">  Dismiss</button>
       </div>
-      <button className="btn btn-link px-0 text-black-50">  Dismiss</button>
+      <div className="listItem white d-flex justify-content-between bg-white py-2 px-3 shadow-sm my-2">
+        <div className="text-left d-flex flex-column justify-content-between ">
+          <div className="listItem__status font-weight-bold"> Warning </div>
+          <div className="text-black-50 listItem__msg" > sdfjsdfjsdfksdflsdf;</div>
+        </div>
+        <button className="btn btn-link px-0 text-black-50">  Dismiss</button>
+      </div>
     </div>
-    </div>
+
   );
 }
 
